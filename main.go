163dbe04196bdc92f6297546d1450a3ba7c4d7f8
bnjix <bnjix@hotmail.com>
2018-08-29 11:51:16 -0700

    PoC for retrieving data points and converting timestamps on the fly

--- main.go
+++ main.go
@@ -1,13 +1,73 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	//"time"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+var db *gorm.DB
+var err error
+
+type DataPoint struct {
+	gorm.Model
+	Timestamp          uint
+	Latitude           float32
+	Longitude          float32
+	Altitude           float32
+	Speed              float32
+	Course             float32
+	Heading            float32
+	VerticalAccuracy   float32
+	HorizontalAccuracy float32
+	TrueHeading        float32
+	AccelerationX      float32
+	AccelerationY      float32
+	AccelerationZ      float32
+	GyroRotationX      float32
+	GyroRotationY      float32
+	GyroRotationZ      float32
+	RelativeAltitude   float32
+	BatteryLevel       float32
+}
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080
+	db, err = gorm.Open("postgres", "host=127.0.0.1 port=5432 user=go_telemetry dbname=go_telemetry password=password sslmode=disable")
+
+	if err != nil {
+		fmt.Println(err)
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
+	// Migrate the schema
+	db.AutoMigrate(&DataPoint{})
+
+	r := gin.Default()
+
+	r.GET("/data_points", GetDataPoints)
+	r.GET("/ping", GetPing)
+	r.Run()
+}
+
+func GetDataPoints(c *gin.Context) {
+	var datapoints []DataPoint
+	if err := db.Find(&datapoints).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		datapoints[0].Course = 1
+		for i := 0; i < len(datapoints); i++{
+			datapoints[i].Timestamp = uint(datapoints[i].CreatedAt.Unix())
+		}
+		c.JSON(200, datapoints)
+	}
+}
+
+func GetPing(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
--- test_db.go
+++ test_db.go
@@ -1,40 +1,65 @@
 package main
 
 import (
-    "fmt"
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/postgres"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
-type Product struct {
-  gorm.Model
-  Code string
-  Price uint
+type DataPoint struct {
+	gorm.Model
+	Timestamp          uint
+	Latitude           float32
+	Longitude          float32
+	Altitude           float32
+	Speed              float32
+	Course             float32
+	Heading            float32
+	VerticalAccuracy   float32
+	HorizontalAccuracy float32
+	TrueHeading        float32
+	AccelerationX      float32
+	AccelerationY      float32
+	AccelerationZ      float32
+	GyroRotationX      float32
+	GyroRotationY      float32
+	GyroRotationZ      float32
+	RelativeAltitude   float32
+	BatteryLevel       float32
 }
 
 func main() {
-  //db, err := gorm.Open("postgres", "host=myhost port=myport user=gorm dbname=gorm password=mypassword")
-  db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=go_telemetry dbname=go_telemetry password=password sslmode=disable")
+	//db, err := gorm.Open("postgres", "host=myhost port=myport user=gorm dbname=gorm password=mypassword")
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=go_telemetry dbname=go_telemetry password=password sslmode=disable")
 
-  if err != nil {
-    panic("failed to connect database")
-  }
-  defer db.Close()
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
 
-  // Migrate the schema
-  db.AutoMigrate(&Product{})
+	// Migrate the schema
+	//db.AutoMigrate(&DataPoint{})
 
-  // Create
-  db.Create(&Product{Code: "L1212", Price: 1000})
+	// Create
+	//db.Create(&DataPoint{Timestamp: 42, Speed: 1000.4})
+  var datapoints []DataPoint
+  fmt.Println(db.Find(&datapoints))
+  fmt.Println(datapoints)
+  //db.Find(&users)
+  //   c.AbortWithStatus(404)
+  //   fmt.Println(err)
+  // } else {
+  //   c.JSON(200, datapoints)
+  // }
+  // fmt.Println(db.First(&product, 1))
+	// Read
+	//var product Product
+	//fmt.Println(db.First(&product, 1)) // find product with id 1
+	//fmt.Println(db.First(&product, "code = ?", "L1212")) // find product with code l1212
 
-  // Read
-  var product Product
-  fmt.Println(db.First(&product, 1)) // find product with id 1
-  fmt.Println(db.First(&product, "code = ?", "L1212")) // find product with code l1212
+	// Update - update product's price to 2000
+	//db.Model(&product).Update("Price", 2000)
 
-  // Update - update product's price to 2000
-  db.Model(&product).Update("Price", 2000)
-
-  // Delete - delete product
-  //db.Delete(&product)
+	// Delete - delete product
+	//db.Delete(&product)
 }
